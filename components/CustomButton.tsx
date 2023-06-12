7e4e2175ef6f9ed2169d0d411f9d6e5c361e49d5
Younesse OUIZANE <younessouizane@gmail.com>
2023-06-12 01:17:44 +0100

    feat: add layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { CustomButtonProps } from "@/types"
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick,btnType}: CustomButtonProps) => {
   return (
     <button 
         disabled = {true}
-        type={"button"}
+        type={btnType || "button"}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
 
