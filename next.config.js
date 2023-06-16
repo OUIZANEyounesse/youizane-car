a00690989094cbd29afe7dd406ebc8fda59e2a4e
Younesse OUIZANE <younessouizane@gmail.com>
2023-06-16 01:27:00 +0100

    fix: egnore error type scripte

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['images.pexels.com'], //Domain of pexels images
       },
+      typescript:{
+        ignoreBuilErrors:true,
+      }
 }
 
 module.exports = nextConfig
